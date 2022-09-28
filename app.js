d079c8b36ce4fe0aac4529a74b763019fe1237de
jfrausto7 <jacob_frausto@brown.edu>
2022-09-27 20:33:04 -0400

    Part 2
    Added JS

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Make a GET request to the fruityvice api to retrieve some fruit data
 const apiRequest = async () => {
-  const BASE_URL = 'https://www.fruityvice.com/api/'
+  const BASE_URL = "https://www.fruityvice.com/api/";
 
   // This endpoint (https://www.fruityvice.com/doc/index.html#api-GET-getAll) returns a list of all the fruits and their info, feel free to play around with different endpoints!
-  const resourcePath = 'fruit/all'
+  const resourcePath = "fruit/all";
 
   // Making a fetch request to an API endpoint
   // Note: a fetch request is an asynchronous operation, and `await` tells the program to wait until the request has been completed before continuing
@@ -11,47 +11,71 @@ const apiRequest = async () => {
   const response = await fetch(buildProxyEndpoint(endpoint), {
     method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-    }
+      "Content-Type": "application/json",
+    },
   });
 
   // console.log(response);
 
   // Return the response in JSON format
   return response.json();
-}
+};
+
+const createTable = (vals) => {
+  var headers = ["Name", "Genus", "Sugar"];
+  var table = document.createElement("TABLE"); //makes a table element for the page
+
+  for (var i = 0; i < vals.length; i++) {
+    var row = table.insertRow(i);
+    row.insertCell(0).innerHTML = vals[i].name;
+    row.insertCell(1).innerHTML = vals[i].genus;
+    row.insertCell(2).innerHTML = vals[i].nutritions.sugar;
+  }
+
+  var header = table.createTHead();
+  var headerRow = header.insertRow(0);
+  for (var i = 0; i < headers.length; i++) {
+    headerRow.insertCell(i).innerHTML = headers[i];
+  }
+
+  const existingElement =
+    document.getElementsByClassName("gallery-container")[0];
+  existingElement.append(table);
+};
 
 const updatePage = async () => {
-  const gallery = document.getElementById('cs1300-gallery');
+  console.log("hi");
+  const gallery = document.getElementById("cs1300-gallery");
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
-  // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
+  // Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  result = fruitsArray.filter((fruit) => {
+    return fruit.nutritions.sugar > 15;
+  });
 
-  // TODO: Create a new HTML element to display your data
-
-  // TODO: Append your new element to the page
-
-}
+  // Create a new HTML element to display your data
+  createTable(result);
+};
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
 const exampleAddElement = () => {
   // Create a new HTML element and set its properties
-  const newElement = document.createElement('div');
+  const newElement = document.createElement("div");
   newElement.innerHTML = "this text is inside a div";
 
   // Append the new element to an existing part of the webpage
-  const existingElement = document.getElementById('example-id');
+  const existingElement = document.getElementById("example-id");
   existingElement.append(newElement);
-}
+};
 
 /**
  * To access information in this API, we need to send our requests through a proxy due to CORS restrictions.
  * This will be used as our proxy to avoid CORS issues.
  */
 // do not touch - stencil code to add the proxy to avoid CORS
-const PROXY_URL = 'https://cs1300-cors-anywhere.herokuapp.com/'
+const PROXY_URL = "https://cs1300-cors-anywhere.herokuapp.com/";
 const buildProxyEndpoint = (endpoint) => `${PROXY_URL}${endpoint}`;
